6071700079973153be0971957781e3b4c20ae8e8
EmiroMunoz01 <emiro_2001@icloud.com>
2022-09-18 18:54:05 -0500

    Arrancamos de nuevo

---
+++ src/main/java/com/emiro/javaonly/JavaOnly.java
@@ -0,0 +1,12 @@
+package com.emiro.javaonly;
+
+/**
+ *
+ * @author Anonimo01
+ */
+public class JavaOnly {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
