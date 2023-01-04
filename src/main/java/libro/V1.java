14e57fadbc1752154ee8c70d159566a4dd98138c
EmiroMunoz01 <emiro_2001@icloud.com>
2023-01-03 22:23:56 -0500

    new

--- src/main/java/libro/V1.java
+++ src/main/java/libro/V1.java
@@ -1,6 +1,7 @@
 package libro;
+
 public class V1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         float x = 144;
         float y = 999;
@@ -10,16 +11,15 @@ public class V1 {
         float division = x / y;
         float multiplicación = x * y;
 
-
-        System.out.println("La suma es: "+suma);
-        System.out.println("La resta es: "+resta);
-        System.out.println("La division es: "+division);
-        System.out.println("La multiplicación es: "+ multiplicación);
-
-
+        System.out.println("La suma es: " + suma);
+        System.out.println("La resta es: " + resta);
+        System.out.println("La division es: " + division);
+        System.out.println("La multiplicación es: " + multiplicación);
 
     }
 }
-// Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
+// Escribe un programa en el que se declaren las variables enteras x e y.
+// Asígnales
 // los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
-// el valor de cada variable, la suma, la resta, la división y la multiplicación.
+// el valor de cada variable, la suma, la resta, la división y la
+// multiplicación.
