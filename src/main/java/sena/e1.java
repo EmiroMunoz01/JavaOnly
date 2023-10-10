5d392778f8fa3e5b10edbabd23d74c51c1581425
EmiroMunoz01 <emiro_2001@icloud.com>
2023-10-10 16:35:14 -0500

    Ejercicio SENA

--- src/main/java/sena/e1.java
+++ src/main/java/sena/e1.java
@@ -1,7 +1,61 @@
 package sena;
 
+import java.util.Scanner;
+
 public class e1 {
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner(System.in);
+
+        double valorradio;
+        double valorarea;
+        double valorperimetro;
+        double valorvolumen;
+
+        System.out.print("\n¿Su figura tiene lados?: \n1.Si \n2.No\n");
+        int base = entrada.nextInt();
+
+        if (base == 1) {
+            System.out.println(
+                    "Su figura tiene lados. Ahora determine si su figura es plana (2d) o tridimencional (3d). Escriba 2d o 3d segun corresponda");
+            int dimension = entrada.nextInt();
+            if (dimension == 1) {
+                System.out.println("Su figura es en 2d, ahora escriba el numero de lados de la figura: ");
+                int lados = entrada.nextInt();
+                if (lados < 3) {
+                    System.out.println(
+                            "Tienes un error, la figura no puede tener menos de 3 lados, vuelve a intentarlo.");
+                } else {
+                    System.out.println("Ingresa las dimensiones de tu figura: ");
+                }
+            }
+        } else if (base == 2) {
+            System.out.println(
+                    "Su figura no tiene lados, entonces es un circulo. Ahora determine si su figura es plana (2d) escriba el número 1 o si es tridimencional (3d) escriba el número 2:");
+            int plano = entrada.nextInt();
+            if (plano == 1) {
+                System.out.println("Su figura es en 2d");
+                System.out.println("========================");
+                System.out.println("Con este software calcularemos el area y perimetro del circulo");
+                System.out.println("========================");
+
+                System.out.println("Ingrese el valor del radio del circulo");
+                int radio = entrada.nextInt();
+                valorarea = (radio * radio) * 3.1416;
+                valorperimetro = 2 * 3.1416 * radio;
+
+                System.out.println("El area del circulo es de: " + valorarea);
+                System.out.println("El perimetro del circulo es de: " + valorperimetro);
+
+            } else if (plano == 2) {
+                System.out.println("Su figura es en 3d");
+                System.out.println("Su figura es en 3d");
+                System.out.println("========================");
+                System.out.println("Con este software calcularemos el area y perimetro del circulo");
+                System.out.println("========================");
+            }
+
+        }
+
     }
+
 }
